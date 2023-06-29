83aae3574aa037eb0ee5f2e774b63e50ff341a74
tkdeshan <tkdeshan1103@gmail.com>
2023-06-29 20:45:10 +0530

    Connect database and complete insert data

--- Tea_Factory_Management_System/Form2.Designer.cs
+++ Tea_Factory_Management_System/Form2.Designer.cs
@@ -28,12 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textMobile = new System.Windows.Forms.TextBox();
+            this.comboArea = new System.Windows.Forms.ComboBox();
+            this.radioMail = new System.Windows.Forms.RadioButton();
+            this.radioFemail = new System.Windows.Forms.RadioButton();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Registration";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.radioFemail);
+            this.groupBox1.Controls.Add(this.radioMail);
+            this.groupBox1.Controls.Add(this.comboArea);
+            this.groupBox1.Controls.Add(this.textMobile);
+            this.groupBox1.Controls.Add(this.textAddress);
+            this.groupBox1.Controls.Add(this.textID);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(28, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(782, 354);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "National ID Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(119, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Mobile Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(119, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Area";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(391, 35);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(282, 26);
+            this.textName.TabIndex = 6;
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(391, 79);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(282, 26);
+            this.textID.TabIndex = 7;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(391, 165);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(282, 26);
+            this.textAddress.TabIndex = 8;
+            // 
+            // textMobile
+            // 
+            this.textMobile.Location = new System.Drawing.Point(391, 209);
+            this.textMobile.Name = "textMobile";
+            this.textMobile.Size = new System.Drawing.Size(282, 26);
+            this.textMobile.TabIndex = 9;
+            // 
+            // comboArea
+            // 
+            this.comboArea.FormattingEnabled = true;
+            this.comboArea.Items.AddRange(new object[] {
+            "Akulahena",
+            "Nakiyadeniya",
+            "Ella",
+            "Visihawulhena",
+            "Aludobahena",
+            "Ipitahena"});
+            this.comboArea.Location = new System.Drawing.Point(391, 254);
+            this.comboArea.Name = "comboArea";
+            this.comboArea.Size = new System.Drawing.Size(282, 28);
+            this.comboArea.TabIndex = 10;
+            // 
+            // radioMail
+            // 
+            this.radioMail.AutoSize = true;
+            this.radioMail.Location = new System.Drawing.Point(391, 123);
+            this.radioMail.Name = "radioMail";
+            this.radioMail.Size = new System.Drawing.Size(61, 24);
+            this.radioMail.TabIndex = 11;
+            this.radioMail.TabStop = true;
+            this.radioMail.Text = "Male";
+            this.radioMail.UseVisualStyleBackColor = true;
+            // 
+            // radioFemail
+            // 
+            this.radioFemail.AutoSize = true;
+            this.radioFemail.Location = new System.Drawing.Point(474, 123);
+            this.radioFemail.Name = "radioFemail";
+            this.radioFemail.Size = new System.Drawing.Size(74, 24);
+            this.radioFemail.TabIndex = 12;
+            this.radioFemail.TabStop = true;
+            this.radioFemail.Text = "Femail";
+            this.radioFemail.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(391, 309);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(134, 39);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(531, 309);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(142, 39);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(837, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer Registration ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioFemail;
+        private System.Windows.Forms.RadioButton radioMail;
+        private System.Windows.Forms.ComboBox comboArea;
+        private System.Windows.Forms.TextBox textMobile;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- Tea_Factory_Management_System/Form2.cs
+++ Tea_Factory_Management_System/Form2.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Tea_Factory_Management_System
 {
@@ -16,5 +19,56 @@ namespace Tea_Factory_Management_System
         {
             InitializeComponent();
         }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-EG4GFB6\\SQLEXPRESS; Initial Catalog=Tea_Factory_Management_System_db; User ID=sa; Password=TkD58630");
+        SqlCommand cmd;
+        SqlDataReader read;
+        string id;
+        bool Mode = true;
+        string sql;
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = textName.Text;
+            string idNum = textID.Text;
+            string gender = string.Empty;
+            if (radioMail.Checked)
+                gender = radioMail.Text;
+            else if (radioFemail.Checked)
+                gender = radioFemail.Text;
+            string address = textAddress.Text;
+            string mobile = textMobile.Text;
+            string area = comboArea.SelectedItem.ToString();
+
+
+            if (Mode == true)
+            {
+                sql = "INSERT INTO customer (name, idNum, gender, address, mobile, area) VALUES (@name, @idNum, @gender, @address, @mobile, @area)";
+                con.Open();
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@idNum", idNum);
+                cmd.Parameters.AddWithValue("@gender", gender);
+                cmd.Parameters.AddWithValue("@address", address);
+                cmd.Parameters.AddWithValue("@mobile", mobile);
+                cmd.Parameters.AddWithValue("@area", area);
+                MessageBox.Show("Succsesfully Recorded!!!");
+                cmd.ExecuteNonQuery();
+
+                textName.Clear();
+                textID.Clear();
+                radioMail.Checked = false;
+                radioFemail.Checked = false;
+                textAddress.Clear();
+                textMobile.Clear();
+                comboArea.SelectedIndex = -1;
+            }
+            else
+            {
+               
+            }
+            con.Close();
+        
+    }
     }
 }
